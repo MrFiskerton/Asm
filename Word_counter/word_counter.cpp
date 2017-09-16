315edb73ccf9dd430cc77a38c9471ef5348311e5
Roman Fiskov <Mr.Fiskerton@yandex.ru>
2017-09-16 13:16:24 +0300

    Full remote save

--- Word_counter/word_counter.cpp
+++ Word_counter/word_counter.cpp
@@ -24,7 +24,7 @@ size_t naive_word_counter (const char* str, size_t size) {
 }
 
 size_t naive_word_counter (const std::string &str) {
-	return naive_word_counter(str.c_str(), str.length());
+    return naive_word_counter(str.c_str(), str.length());
 }
 
 size_t asm_word_counter (const char* str, size_t size) {
@@ -102,73 +102,73 @@ size_t asm_word_counter (const char* str, size_t size) {
 }
 
 size_t asm_word_counter (const std::string &str) {
-	return asm_word_counter(str.c_str(), str.length());
+    return asm_word_counter(str.c_str(), str.length());
 }
 
 std::string random_string_generator (size_t min_len = 1, size_t max_len = 10000, size_t number_of_letters_for_one_space = 8) {
-	std::cout << "\nStarting generate random string ( size := "; 
+    std::cout << "\nStarting generate random string ( size := "; 
 
-	std::stringstream result;
-	size_t len;
-	
-	std::cout << ( len = std::rand() % (max_len - min_len + 1) + min_len ) << " ) ... ";
-	std::flush(std::cout);
+    std::stringstream result;
+    size_t len;
+    
+    std::cout << ( len = std::rand() % (max_len - min_len + 1) + min_len ) << " ) ... ";
+    std::flush(std::cout);
 
-	bool is_space;
-	for(size_t i = 0; i < len; i++) {
-		is_space = std::rand() % (number_of_letters_for_one_space + 1) == 0;
-		result << char(is_space ? ' ' : 'a' + std::rand() % 26);
-	}
+    bool is_space;
+    for(size_t i = 0; i < len; i++) {
+        is_space = std::rand() % (number_of_letters_for_one_space + 1) == 0;
+        result << char(is_space ? ' ' : 'a' + std::rand() % 26);
+    }
 
-	std::cout << "Finish.\n\n";
+    std::cout << "Finish.\n\n";
 
-	return result.str();
+    return result.str();
 }
 
 double call_with_timer (const std::string header, const std::string &arg, size_t ( *test_f )(const std::string&), int &result) {
-	std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
-	std::chrono::duration<double> elapsed;
+    std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
+    std::chrono::duration<double> elapsed;
 
-	std::cout << header;
-	start = std::chrono::high_resolution_clock::now();
-	result = test_f(arg);
-	end = std::chrono::high_resolution_clock::now();
+    std::cout << header;
+    start = std::chrono::high_resolution_clock::now();
+    result = test_f(arg);
+    end = std::chrono::high_resolution_clock::now();
 
-	double time = (elapsed = end - start).count();
-	std::cout << time << std::endl;
+    double time = (elapsed = end - start).count();
+    std::cout << time << std::endl;
 
-	return time;
+    return time;
 }
 
 bool test_word_counter(int N = 100, int max_str_len = (int)1e5) {
-	std::string test_str;
-	double naive_time(0), asm_time(0);
-	int naive_result, asm_result;
+    std::string test_str;
+    double naive_time(0), asm_time(0);
+    int naive_result, asm_result;
 
-	while (N--) {
-		test_str = random_string_generator(1e1, max_str_len, 8);
+    while (N--) {
+        test_str = random_string_generator(1e1, max_str_len, 8);
 
-		naive_time += call_with_timer("Naive time: ", test_str, naive_word_counter, naive_result);	
-		asm_time   += call_with_timer("Asm time:   ", test_str, asm_word_counter,   asm_result);
+        naive_time += call_with_timer("Naive time: ", test_str, naive_word_counter, naive_result);	
+        asm_time   += call_with_timer("Asm time:   ", test_str, asm_word_counter,   asm_result);
 
-		if (naive_result != asm_result) {
-			std::cerr << " >>> There is an error. Number of words not equal.\nTest := " << test_str << std::endl;
-			std::cerr << "\ntest_size    := " << test_str.length() << " chars"
-					  << "\nnaive_result := " << naive_result
-					  << "\nasm_result   := " << asm_result
-					  << std::endl;
-			return false;
-		}
-	}		
+        if (naive_result != asm_result) {
+            std::cerr << " >>> There is an error. Number of words not equal.\nTest := " << test_str << std::endl;
+            std::cerr << "\ntest_size    := " << test_str.length() << " chars"
+                      << "\nnaive_result := " << naive_result
+                      << "\nasm_result   := " << asm_result
+                      << std::endl;
+            return false;
+        }
+    }
 
-	std::cout << "-------------------------------------------------\n"
-			  << " >>> Boost: " << naive_time / asm_time 
-			  << std::endl;
-	return true;
+    std::cout << "-------------------------------------------------\n"
+              << " >>> Boost: " << naive_time / asm_time 
+              << std::endl;
+    return true;
 }
 
 int main (int argc, char* argv[]) {
-	std::srand(std::time(0));
+    std::srand(std::time(0));
     
     if (argc == 2 && std::string(argv[1]) == "-stress") {
         test_word_counter(1, (int)1e9);
@@ -176,5 +176,5 @@ int main (int argc, char* argv[]) {
         test_word_counter(3);
     }
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
